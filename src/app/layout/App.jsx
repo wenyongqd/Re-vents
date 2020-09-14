b0951ce89567429707dea0c6293a5f6e76dea07a
wenyongqd <wenyongqd@gmail.com>
2020-09-14 09:08:39 -0400

    update

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import EventDashboard from '../..//features/events/eventDashboard/EventDashboard'
+import NavBar from '../../features/events/nav/NavBar'
+import { Container } from 'semantic-ui-react'
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div>
-      <h1>Re-events</h1>
-      <EventDashboard />
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen}/>
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+      </Container>
+    </>
   );
 }
 
--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import EventList from "./EventList";
+import EventForm from "../eventForm/EventForm";
+import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard() {
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <h2>Left Column</h2>
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <h2>Right Column</h2>
-            </Grid.Column>
-        </Grid>
-    )
+export default function EventDashboard({formOpen, setFormOpen}) {
+    const [events, setEvents] = useState(sampleData);
+    
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+          {formOpen && 
+          <EventForm setFormOpen={setFormOpen}/>}
+      </Grid.Column>
+    </Grid>
+  );
 }
